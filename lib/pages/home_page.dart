7206dd1857fa2d67e873ea2c4427e96ba850df77
saeedbm <iamsaeedbm@gmail.com>
2025-07-25 23:06:26 +0330

    modify add task button

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sql_crud/widgets/add_task.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,6 +12,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: addTaskButton(),
       body: SafeArea(child: Column(children: [Text('data')])),
     );
   }
